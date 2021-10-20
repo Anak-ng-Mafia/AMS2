054f601291e58c5eca4607375f5898cc0d2d5992
stefanomhel30 <90842996+stefanomhel30@users.noreply.github.com>
2021-10-20 17:54:06 +0800

    Add files via upload

---
+++ VETIFY_AMS_FINAL/AMS2-main/config.js
@@ -0,0 +1,13 @@
+
+ const firebaseConfig = {
+                        apiKey: "AIzaSyD_iHrYN1ink0vCTIJvxmicGVuRddNzRHU",
+                        authDomain: "vetify-2602e.firebaseapp.com",
+                        projectId: "vetify-2602e",
+                        storageBucket: "vetify-2602e.appspot.com",
+                        messagingSenderId: "534160187138",
+                        appId: "1:534160187138:web:d90483f411ecbb27a56c1b",
+                        measurementId: "G-36H2DNHT1Y"
+                        };
+                      
+                          firebase.initializeApp(firebaseConfig);
+                      
